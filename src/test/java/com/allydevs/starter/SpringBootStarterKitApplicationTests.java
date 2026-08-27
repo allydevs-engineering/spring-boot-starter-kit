7946e107a58036a645b53df6bc6785f9f63d44f2
Admin AllyDevs <admin@allydevs.com>
2026-08-27 16:25:15 +0530

    chore: initialize spring boot starter

---
+++ src/test/java/com/allydevs/starter/SpringBootStarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allydevs.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
